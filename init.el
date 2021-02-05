846f621d38e40f873a6a0562a3e4dd2cf8356533
Xairi <xairi.valdivia@gmail.com>
2021-02-05 17:06:10 -0500

    WIP: create org file

--- init.el
+++ init.el
@@ -6,3 +6,23 @@
 (package-initialize)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; use-package
+;; https://github.com/jwiegley/use-package/tree/317137b07687f16ea97a2493b0a6768c9df381a0
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
