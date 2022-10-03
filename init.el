c107dd690b128473ce123a031cc4eff626f7941d
prometeo <prometeo@gmail.com>
2022-10-03 16:12:32 -0500

    feat: install terraform mode

--- init.el
+++ init.el
@@ -22,7 +22,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
- '(package-selected-packages '(doom-modeline doom-themes all-the-icons use-package)))
+ '(package-selected-packages
+   '(terraform-mode doom-modeline doom-themes all-the-icons use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
