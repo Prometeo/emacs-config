e0d62a532a9edd23ca7fe1c48e0137cccdeb44d7
Prometeo <xairi.valdivia@gmail.com>
2024-10-21 18:46:35 -0500

    feat: Finish new emacs config with elpaca

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;; Ensure that use-package is installed.
 ;;
+
 ;; If use-package isn't already installed, it's extremely likely that this is a
 ;; fresh installation! So we'll want to update the package repository and
 ;; install use-package before loading the literate configuration.
