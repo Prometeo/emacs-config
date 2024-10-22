2dfce581c21ba4d5f24afc7de1f6e559d3024b95
Prometeo <xairi.valdivia@gmail.com>
2024-10-21 21:46:17 -0500

    feat: Ensure package is loaded after elpaca

--- init.el
+++ init.el
@@ -1,15 +1,13 @@
 ;; Ensure that use-package is installed.
 ;;
-
+;;(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+(org-babel-load-file "~/.emacs.d/README.org")
 ;; If use-package isn't already installed, it's extremely likely that this is a
 ;; fresh installation! So we'll want to update the package repository and
 ;; install use-package before loading the literate configuration.
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
-
-;;(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
-(org-babel-load-file "~/.emacs.d/README.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
