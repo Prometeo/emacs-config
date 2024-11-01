a740107b381994b9c30dcf85a72cac50f6f1f4fa
Prometeo <xairi.valdivia@gmail.com>
2024-11-01 16:58:46 -0500

    feat: Update org mode configuration

--- init.el
+++ init.el
@@ -13,9 +13,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-mode-line-update-function #'my/anzu-update-func)
+ '(anzu-replace-threshold 50)
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
  '(custom-safe-themes
    '("7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
      default))
+ '(org-agenda-files (list org-directory))
+ '(org-directory "~/Documents/org")
  '(package-selected-packages '(evil transient)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
