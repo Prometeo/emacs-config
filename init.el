4eb2193baca9377972e1b1ff45acc0cd7f61e7cc
= <=>
2026-06-20 20:36:13 -0500

    feat: move ibuffer with casual configuration to casual section

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
+   '("a6920ee8b55c441ada9a19a44e9048be3bfb1338d06fc41bce3819ac22e4b5a1"
+     "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
      default))
  '(org-agenda-files (list org-directory))
  '(org-directory "~/Documents/org")
